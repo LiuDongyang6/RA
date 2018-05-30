76f04a1c0ec6d87bd8933d93412946100645c182
Abingcbc <1999062.love@163.com>
2018-05-30 22:06:58 +0800

    tiled_map完成

--- Classes/TiledMap/tiled_map.cpp
+++ Classes/TiledMap/tiled_map.cpp
@@ -15,9 +15,10 @@ const float RAMap::speed = 50;
 const int RAMap::accurancy = 50;
 TMXTiledMap * RAMap::_tiledMap;
 Point RAMap::diff;
-std::map<cocos2d::Point, bool> RAMap::collision;
-std::map<cocos2d::Point, bool> RAMap::oil;
-
+std::map<Point, bool> RAMap::collision;
+std::map<Point, bool> RAMap::oil;
+std::vector<Point> RAMap::pre_positions;
+std::vector<Point> RAMap::dests;
 
 // on "init" you need to initialize your instance
 bool RAMap::init()
@@ -71,7 +72,6 @@ void RAMap::testForCoord(void) {
 }
 
 // OpenGL坐标转换为Tiledmap坐标
-
 Point RAMap::glCoordToTileCoord(Point gl_coord) {
 	//求map原点坐标  
 	int map_width = _tiledMap->getMapSize().width;
@@ -106,7 +106,6 @@ Point RAMap::glCoordToTileCoord(Point gl_coord) {
 		n = static_cast<int>(n);
 	return Point(m, n);
 }
-
 
 //设置地图移动方向
 void RAMap::setMovePosition(void) {
@@ -176,10 +175,8 @@ void RAMap::moveMap(float dt) {
 	node->setPosition(currentPos + speed * diff);
 }
 
-
 //判断是否可以建造普通建筑
-
-bool RAMap::cannotBuildNormal(cocos2d::Point build_point/*GL 坐标*/, int size) {
+bool RAMap::cannotBuildNormal(Point build_point/*GL 坐标*/, int size) {
 	//从GL坐标转化为瓦片坐标  
 	Point tile_coord = glCoordToTileCoord(build_point);
 	if (tile_coord.x <= 0)
@@ -197,9 +194,8 @@ bool RAMap::cannotBuildNormal(cocos2d::Point build_point/*GL 坐标*/, int size)
 	return true;
 }
 
-
 //判断是否可以建造油井
-Point RAMap::cannotBuildOil(cocos2d::Point build_point, int size) {
+Point RAMap::cannotBuildOil(Point build_point, int size) {
 	//从GL坐标转化为瓦片坐标  
 	Point tile_coord = glCoordToTileCoord(build_point);
 	if (tile_coord.x <= 0)
@@ -219,7 +215,7 @@ Point RAMap::cannotBuildOil(cocos2d::Point build_point, int size) {
 }
 
 //确定建造普通建筑
-void RAMap::sureToBuildNormal(cocos2d::Point build_point, int size) {
+void RAMap::sureToBuildNormal(Point build_point, int size) {
 	Point tile_coord = glCoordToTileCoord(build_point);
 	for (int x = 0; x < size; x++) {
 		for (int y = 0; y < size; y++) {
@@ -232,8 +228,8 @@ void RAMap::sureToBuildNormal(cocos2d::Point build_point, int size) {
 	}
 }
 
-//确定建造油井
-void RAMap::sureToBuildOil(cocos2d::Point build_point, int size) {
+//确定建造油井 传入的point是cannotBuildOil返回的
+void RAMap::sureToBuildOil(Point build_point, int size) {
 	Point tile_coord = glCoordToTileCoord(build_point);
 	for (int x = 0; x < size; x++) {
 		for (int y = 0; y < size; y++) {
@@ -245,3 +241,155 @@ void RAMap::sureToBuildOil(cocos2d::Point build_point, int size) {
 	}
 }
 
+//普通建筑被摧毁
+void RAMap::destroyNormalBuildings(Point des_pos, int size) {
+	Point tile_coord = glCoordToTileCoord(des_pos);
+	for (int x = 0; x < size; x++) {
+		for (int y = 0; y < size; y++) {
+			collision[tile_coord] = 0;
+			tile_coord.y--;
+
+		}
+		tile_coord.y += size;
+		tile_coord.x--;
+	}
+}
+
+//油田被摧毁
+void RAMap::destroyOilBuildings(Point des_pos, int size) {
+	Point tile_coord = glCoordToTileCoord(des_pos);
+	for (int x = 0; x < size; x++) {
+		for (int y = 0; y < size; y++) {
+			oil[tile_coord] = 1;
+			tile_coord.y--;
+
+		}
+		tile_coord.y += size;
+		tile_coord.x--;
+	}
+}
+
+//士兵占据这格
+void RAMap::setSoilderCollision(Point pos) {
+	Point tile_coord = glCoordToTileCoord(pos);
+	collision[tile_coord] = 1;
+}
+
+//士兵离开这格
+void RAMap::removeSoilderCollision(cocos2d::Point pos) {
+	Point tile_coord = glCoordToTileCoord(pos);
+	collision[tile_coord] = 0;
+}
+
+//不想解释
+std::map<Point, int> RAMap::tryEightdirection(Point position, Point dest) {
+	std::map<Point, int> open_list;
+	if (collision[Point(position.x - 1, position.y)])//1
+		open_list[Point(position.x - 1, position.y)] = 10000;
+	else
+		open_list[Point(position.x - 1, position.y)] =
+		10 + 10 * (abs(dest.x - position.x + 1) + abs(dest.y - position.y));
+	if (collision[Point(position.x + 1, position.y)])//2
+		open_list[Point(position.x + 1, position.y)] = 10000;
+	else
+		open_list[Point(position.x + 1, position.y)] =
+		10 + 10 * (abs(dest.x - position.x - 1) + abs(dest.y - position.y));
+	if (collision[Point(position.x, position.y - 1)])//3
+		open_list[Point(position.x, position.y - 1)] = 10000;
+	else
+		open_list[Point(position.x, position.y - 1)] =
+		10 + 10 * (abs(dest.x - position.x) + abs(dest.y - position.y + 1));
+	if (collision[Point(position.x, position.y + 1)])//4
+		open_list[Point(position.x, position.y + 1)] = 10000;
+	else
+		open_list[Point(position.x, position.y + 1)] =
+		10 + 10 * (abs(dest.x - position.x) + abs(dest.y - position.y - 1));
+	if (collision[Point(position.x + 1, position.y + 1)])//5
+		open_list[Point(position.x + 1, position.y + 1)] = 10000;
+	else
+		open_list[Point(position.x + 1, position.y + 1)] =
+		14 + 10 * (abs(dest.x - position.x - 1) + abs(dest.y - position.y - 1));
+	if (collision[Point(position.x - 1, position.y - 1)])//6
+		open_list[Point(position.x - 1, position.y - 1)] = 10000;
+	else
+		open_list[Point(position.x - 1, position.y - 1)] =
+		14 + 10 * (abs(dest.x - position.x + 1) + abs(dest.y - position.y + 1));
+	if (collision[Point(position.x - 1, position.y + 1)])//7
+		open_list[Point(position.x - 1, position.y + 1)] = 10000;
+	else
+		open_list[Point(position.x - 1, position.y + 1)] =
+		14 + 10 * (abs(dest.x - position.x + 1) + abs(dest.y - position.y - 1));
+	if (collision[Point(position.x + 1, position.y - 1)])//8
+		open_list[Point(position.x + 1, position.y - 1)] = 10000;
+	else
+		open_list[Point(position.x + 1, position.y - 1)] =
+		14 + 10 * (abs(dest.x - position.x - 1) + abs(dest.y - position.y + 1));
+	return open_list;
+}
+
+//寻路
+std::map<Point, Point> RAMap::findRoutine(std::vector<Point> positions, Point dest) {
+	std::map<Point, Point> routine;
+	if (dests[0] != dest) {
+		pre_positions = positions;
+		dests.clear();
+		dests.push_back(dest);
+	}//如果选定了新的终点
+	dest = *(dests.end() - 1);//获取新的终点
+	for (auto position : positions) {
+		if (find(dests.begin(), dests.end() - 1, position) != dests.end()) {
+			routine[position] = position;
+		}
+		auto position_tile = glCoordToTileCoord(position);
+		auto dest_tile = glCoordToTileCoord(dest);
+		auto open_list = tryEightdirection(position_tile, dest_tile);
+		auto next_step = open_list.begin();
+		auto start = open_list.begin();
+		for (start; start != open_list.end(); start++) {
+			if (start->second < next_step->second)
+				next_step = start;
+		}
+		if (next_step->first == dest_tile) {//dest的collision一定是0
+			routine[position] = dest;
+			for (int i = 0; i + position_tile.x < 128; i++) {
+				auto open_list_2 = tryEightdirection(Point(position_tile.x + i, position_tile.y),
+					Point(0, 0));
+				auto next_dest = open_list_2.begin();
+				auto start_2 = open_list_2.begin();
+				for (start_2; start_2 != open_list_2.end(); start_2++) {
+					if (start_2->second < next_dest->second)
+						next_dest = start_2;
+				}
+				if (next_dest->second != 10000) {
+					dests.push_back(_tiledMap->getLayer("collision")->getPositionAt(next_dest->first));
+					dest = _tiledMap->getLayer("collision")->getPositionAt(next_dest->first);
+					dest_tile = glCoordToTileCoord(dest);
+					break;
+				}
+			}
+			if (dest_tile == position_tile) {//运气很背，前面都没找到的话
+				for (int i = 0; i + position_tile.x > 0; i--) {
+					auto open_list_2 = tryEightdirection(Point(position_tile.x + i, position_tile.y),
+						Point(0, 0));
+					auto next_dest = open_list_2.begin();
+					auto start_2 = open_list_2.begin();
+					for (start_2; start_2 != open_list_2.end(); start_2++) {
+						if (start_2->second < next_dest->second)
+							next_dest = start_2;
+					}
+					if (next_dest->second != 10000) {
+						dests.push_back(_tiledMap->getLayer("collision")->getPositionAt(next_dest->first));
+						dest = _tiledMap->getLayer("collision")->getPositionAt(next_dest->first);
+						break;
+					}
+				}
+			}
+			continue;
+		}
+		if (next_step->second == 10000) 
+			routine[position] = position;
+		else 
+			routine[position] = _tiledMap->getLayer("collision")->getPositionAt(next_step->first);
+	}
+	return routine;
+}
