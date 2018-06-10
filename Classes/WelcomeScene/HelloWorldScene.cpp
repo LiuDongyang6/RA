c3ff5c6ba8a41707e6fb8a931be181910b9a283f
卜滴 <997219957@qq.com>
2018-06-10 13:39:14 +0800

    welcome scene

---
+++ Classes/WelcomeScene/HelloWorldScene.cpp
@@ -0,0 +1,216 @@
+#include "HelloWorldScene.h"
+#include "SimpleAudioEngine.h"
+#include "SettingScene.h"
+#include "StartScene.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+	// 'scene' is an autorelease object
+	auto scene = Scene::create();
+
+	// 'layer' is an autorelease object
+	auto layer = HelloWorld::create();
+
+	// add layer as a child to scene
+	scene->addChild(layer);
+
+	// return the scene
+	return scene;
+}
+
+// Print useful error message instead of segfaulting when files are not there.
+static void problemLoading(const char* filename)
+{
+    printf("Error while loading: %s\n", filename);
+    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    /////////////////////////////
+    // 2. add a menu item with "X" image, which is clicked to quit the program
+    //    you may modify it.
+
+	Sprite *bg = Sprite::create("background.png");
+
+	// position the label on the center of the screen
+	bg->setPosition(Vec2(origin.x + visibleSize.width / 2,
+		origin.y + visibleSize.height / 2));
+	Size mywinsize = Director::getInstance()->getWinSize();
+	float winw = mywinsize.width;
+	float winh = mywinsize.height;
+	float spx = bg->getTextureRect().getMaxX();
+	float spy = bg->getTextureRect().getMaxY();
+	bg->setScaleX(winw / spx);
+	bg->setScaleY(winw / spx);
+	this->addChild(bg);
+
+	// start sprite
+	Sprite* startSpriteNormal = Sprite::create("start-up1.png");
+	Sprite* startSpriteSelected = Sprite::create("start-down1.png");
+
+	MenuItemSprite * startMenuItem = MenuItemSprite::create(startSpriteNormal,
+		startSpriteSelected,
+		CC_CALLBACK_1(HelloWorld::menuItemStartCallback, this));
+	startMenuItem->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height * 2 / 3));
+
+	// set image menu
+	MenuItemImage* settingMenuItem = MenuItemImage::create(
+		"setting-up1.png",
+		"setting-down1.png",
+		CC_CALLBACK_1(HelloWorld::menuItemSettingCallback, this));
+	settingMenuItem->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2));
+
+	// help menu
+	MenuItemImage* helpMenuItem = MenuItemImage::create(
+		"help-up1.png",
+		"help-down1.png",
+		CC_CALLBACK_1(HelloWorld::menuItemHelpCallback, this));
+	helpMenuItem->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 3));
+
+	Menu* mu = Menu::create(startMenuItem, settingMenuItem, helpMenuItem, NULL);
+	mu->setPosition(Vec2::ZERO);
+	this->addChild(mu,1);
+	
+    // add a "close" icon to exit the progress. it's an autorelease object
+    auto closeItem = MenuItemImage::create(
+                                           "CloseNormal1.png",
+                                           "CloseSelected1.png",
+                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+
+    if (closeItem == nullptr ||
+        closeItem->getContentSize().width <= 0 ||
+        closeItem->getContentSize().height <= 0)
+    {
+        problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
+    }
+    else
+    {
+        float x = origin.x + visibleSize.width - closeItem->getContentSize().width/2;
+        float y = origin.y + closeItem->getContentSize().height/2;
+        closeItem->setPosition(Vec2(x,y));
+    }
+
+    // create menu, it's an autorelease object
+    auto menu = Menu::create(closeItem, NULL);
+    menu->setPosition(Vec2::ZERO);
+    this->addChild(menu, 1);
+
+    /////////////////////////////
+    // 3. add your codes below...
+
+    // add a label shows "Red Alert"
+    // create and initialize a label
+
+    auto label = Label::createWithTTF("Red Alert", "fonts/Marker Felt.ttf", 24);
+    if (label == nullptr)
+    {
+        problemLoading("'fonts/Marker Felt.ttf'");
+    }
+    else
+    {
+        // position the label on the center of the screen
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height - label->getContentSize().height));
+
+        // add the label as a child to this layer
+        this->addChild(label, 1);
+    }
+
+    // add "HelloWorld" splash screen"
+    auto sprite = Sprite::create("HelloWorld.png");
+    if (sprite == nullptr)
+    {
+        problemLoading("'HelloWorld.png'");
+    }
+    else
+    {
+        // position the sprite on the center of the screen
+        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+
+        // add the sprite as a child to this layer
+        this->addChild(sprite, 0);
+    }
+
+	/* 加载UI */
+	auto UI = cocostudio::GUIReader::getInstance()->widgetFromJsonFile("HelloUI_1.ExportJson");
+	UI->setPosition(Vec2(100, 100));
+	this->addChild(UI);
+	
+	/* 获取控件对象 */
+	Button* xiaoruoBtn = (Button*)Helper::seekWidgetByName(UI, "xiaoruoBtn");
+	m_xiaoruoImg = (ImageView*)Helper::seekWidgetByName(UI, "xiaoruoImg");
+
+	/* 添加按钮单击监听 */
+	xiaoruoBtn->addTouchEventListener(this, toucheventselector(HelloWorld::onClick));
+
+    return true;
+}
+
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+    //Close the cocos2d-x game scene and quit the application
+    Director::getInstance()->end();
+
+    #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+
+    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() and exit(0) as given above,instead trigger a custom event created in RootViewController.mm as below*/
+
+    //EventCustom customEndEvent("game_scene_close_event");
+    //_eventDispatcher->dispatchEvent(&customEndEvent);
+
+
+}
+
+void HelloWorld::onClick(Ref*, TouchEventType type) {
+	switch (type) {
+	case TouchEventType::TOUCH_EVENT_BEGAN: // 单机事件开始时触发（按下的时候）
+		break;
+	case TouchEventType::TOUCH_EVENT_MOVED: // 按下之后，进行移动操作时触发
+		break;
+	case TouchEventType::TOUCH_EVENT_ENDED: // 按下之后，然后松开时触发
+		if (m_xiaoruoImg->isVisible()) {
+			m_xiaoruoImg->setVisible(false);
+		}
+		else {
+			m_xiaoruoImg->setVisible(true);
+		}
+		break;
+	case TouchEventType::TOUCH_EVENT_CANCELED: // 因为一些特殊情况中断时触发
+		break;
+	}
+}
+
+void HelloWorld::menuItemSettingCallback(Ref* pSender)
+{
+	auto sc = Setting::createScene();
+	Director::getInstance()->pushScene(sc);
+}
+
+void HelloWorld::menuItemHelpCallback(Ref* pSender)
+{
+	MenuItem* item = (MenuItem*)pSender;
+	log("Touch Help %p", item);
+}
+
+void HelloWorld::menuItemStartCallback(Ref* pSender)
+{
+	auto sc = Start::createScene();
+	Director::getInstance()->pushScene(sc);
+}
